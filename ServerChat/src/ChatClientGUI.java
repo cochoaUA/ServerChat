673ff7e1bb6dfe34d667fcb8a077c80b6ee35383
zuniga7 <zuniga7@email.arizona.edu>
2013-03-26 14:34:46 -0700

    - GUI is looking better

--- ServerChat/src/ChatClientGUI.java
+++ ServerChat/src/ChatClientGUI.java
@@ -1,8 +1,21 @@
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class ChatClientGUI {
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
-	public void main(String [] args){
-		
+
+public class ChatClientGUI extends JFrame{
+
+	
+	private JTextArea chatArea = new JTextArea();
+	private JTextField messageField = new JTextField();
+	
+	public static void main(String [] args){
+		JFrame gui = new ChatClientGUI();
+		gui.setVisible(true);
 	}
 	
 	public ChatClientGUI(){
@@ -15,7 +28,7 @@ public class ChatClientGUI {
 	 */
 	private void setUpListeners() {
 		// TODO 1:46:48 PM complete this method
-		
+		SendMessageListener listener = new SendMessageListener();
 	}
 
 	/**
@@ -23,6 +36,27 @@ public class ChatClientGUI {
 	 */
 	private void layoutView() {
 		// TODO 1:46:45 PM complete this method
+		this.setTitle("Chat Client");
+		this.setSize(500, 500);
+		
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		this.add(messageField, BorderLayout.NORTH);
+		this.add(chatArea, BorderLayout.CENTER);
+		
+	}
+	
+	private class SendMessageListener implements ActionListener{
+
+		/* (non-Javadoc)
+		 * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+		 */
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			// TODO 2:34:01 PM complete this method
+			
+		}
 		
 	}
 }
