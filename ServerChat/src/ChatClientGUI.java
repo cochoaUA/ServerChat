427b6e68e7ab2cebc31217c85725e63451d6995e
zuniga7 <zuniga7@email.arizona.edu>
2013-03-26 13:48:19 -0700

    - getting somewhere haha

---
+++ ServerChat/src/ChatClientGUI.java
@@ -0,0 +1,28 @@
+
+public class ChatClientGUI {
+
+	public void main(String [] args){
+		
+	}
+	
+	public ChatClientGUI(){
+		layoutView();
+		setUpListeners();
+	}
+
+	/**
+	 * 
+	 */
+	private void setUpListeners() {
+		// TODO 1:46:48 PM complete this method
+		
+	}
+
+	/**
+	 * 
+	 */
+	private void layoutView() {
+		// TODO 1:46:45 PM complete this method
+		
+	}
+}
